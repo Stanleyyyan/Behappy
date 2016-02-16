3fd3833069b59a98a311cb46fca3ebddfc5251ad
Zichen Wang <wangzichen530@hotmail.com>
2016-02-16 10:31:40 -0600

    Update MainActivity
    add a button

--- app/src/main/java/com/stanley/myapplication/MainActivity.java
+++ app/src/main/java/com/stanley/myapplication/MainActivity.java
@@ -1,13 +1,29 @@
 package com.stanley.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_to_map;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_to_map = (Button) findViewById(R.id.btn_to_map);
+        btn_to_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LocationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
