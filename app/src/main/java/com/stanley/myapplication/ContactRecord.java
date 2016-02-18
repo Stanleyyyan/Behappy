560c99c40cfe4545819f486f8a04f6e4a4392df7
Zichen Wang <wangzichen530@hotmail.com>
2016-02-17 19:59:42 -0600

    Create ContactRecord.java

---
+++ app/src/main/java/com/stanley/myapplication/ContactRecord.java
@@ -0,0 +1,44 @@
+package com.stanley.myapplication.contactrecord;
+
+/**
+ * Created by XieDugu on 2016/2/13.
+ */
+public class ContactRecord {
+
+    private int contrcdId;
+    private String contactName;
+    private String contrcdDateTime;//--MM-dd hh:mm
+    private int contrcdType;
+    //--1: incoming
+    //--2: outgoing
+    //--3: miss
+
+    public int getContrcdId() {
+        return contrcdId;
+    }
+    public void setContrcdId(int contrcdId) {
+        this.contrcdId = contrcdId;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContrcdDateTime() {
+        return contrcdDateTime;
+    }
+    public void setContrcdDateTime(String contrcdDate) {
+        this.contrcdDateTime = contrcdDate;
+    }
+
+    public int getContrcdType() {
+        return contrcdType;
+    }
+    public void setContrcdType(int contrcdType) {
+        this.contrcdType = contrcdType;
+    }
+
+}
